76024c4b6de64e26fd994b1642cacf30e384f990
Rushabh Shroff <rushabh.s@browserstack.com>
2023-10-03 12:34:35 +0530

    Initial Commit

---
+++ test/sample_test.js
@@ -0,0 +1,27 @@
+var assert = require('assert');
+const { Builder, By, Capabilities, until } = require("selenium-webdriver");
+const { percyScreenshot } = require('@percy/selenium-webdriver');
+var buildDriver = function() {
+  return new Builder().
+    usingServer('http://localhost:4444/wd/hub').
+    withCapabilities(Capabilities.chrome()).
+    build();
+};
+const baseUrl = 'https://browserstack.com'
+describe('Sample Percy Test', async function() {
+  this.timeout(0);
+  var driver;
+
+  before(function() {
+    driver = buildDriver();
+  });
+
+  it('Homepage Snapshot', async function () {
+    await driver.get(baseUrl);
+    await percyScreenshot(driver, 'BrowserStack Homepage');
+  });
+
+  after(async function() {
+    await driver.quit();
+  });
+});
